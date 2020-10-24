6bab8bea7450c9b8142d19e25974f42bf59fecb3
Huy Phan <huyphan0823@gmail.com>
2020-10-24 15:51:44 -0500

    Fix gui

--- src/gui.py
+++ src/gui.py
@@ -13,29 +13,21 @@ class GUI():
         
         self.txt_edit = tk.Text(self.root)
         self.fr_buttons = tk.Frame(self.root, relief=tk.RAISED, bd=2)
-        self.btn_open = tk.Button(self.fr_buttons, text="Open", command=lambda: open_file())
-        self.btn_save = tk.Button(self.fr_buttons, text="Save As...", command=lambda: save_file(self))
+        self.btn_start = tk.Button(self.fr_buttons, text="Start", command=lambda: on_btn_start_click(self))
+        self.btn_stop = tk.Button(self.fr_buttons, text="Stop", command=lambda: on_btn_stop_click(self))
 
-        self.btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-        self.btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+        self.btn_start.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_stop.grid(row=1, column=0, sticky="ew", padx=5)
 
         self.fr_buttons.grid(row=0, column=0, sticky="ns")
         self.txt_edit.grid(row=0, column=1, sticky="nsew")
 
 
-def open_file():
+def on_btn_start_click(self):
     start()
 
+def on_btn_stop_click(self):
+    self.root.destroy()
+    stop()
 
-def save_file(self):
-    """Save the current file as a new file."""
-    filepath = asksaveasfilename(
-        defaultextension="txt",
-        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
-    )
-    if not filepath:
-        return
-    with open(filepath, "w") as output_file:
-        text = self.txt_edit.get(1.0, tk.END)
-        output_file.write(text)
-    self.root.title(f"Simple Text Editor - {filepath}")
+
